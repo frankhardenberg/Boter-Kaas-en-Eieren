43eb88b0f3e4ab5bd868d4715a4f8fe21c237767
Frank Otten <34128958+frankhardenberg@users.noreply.github.com>
2018-10-02 15:03:17 +0200

    Update
    Update

--- Boter-Kaas-en-Eieren/TicTacToe.cs
+++ Boter-Kaas-en-Eieren/TicTacToe.cs
@@ -17,8 +17,16 @@ namespace Boter_Kaas_en_Eieren
             InitializeComponent();
         }
 
+        string A;
+        string B;
+        string C;
+        string Row1;
+        string Row2;
+        string Row3;
+
         bool Turn = true;
         int TurnCount = 0;
+        List<string> Alghoritmes = new List<string>();
 
         private void Letter_Click(object sender, EventArgs e)
         {
@@ -38,14 +46,54 @@ namespace Boter_Kaas_en_Eieren
             TurnCount++;
         }
 
-        public void RowsAndColums()
+        public void WinnaarsCheck()
         {
-            
+            if (A1.Text == "X" && A2.Text == "X" && A3.Text == "X")
+            {
+                MessageBox.Show("Player 'X' wins!");                
+            }
+
+            if (B1.Text == "X" && B2.Text == "X" && B3.Text == "X")
+            {
+                MessageBox.Show("Player 'X' wins!");
+            }
+
+            if (C1.Text == "X" && C2.Text == "X" && C3.Text == "X")
+            {
+                MessageBox.Show("Player 'X' wins!");
+            }
+
+            if (A1.Text == "X" && B2.Text == "X" && C3.Text == "X" || C1.Text == "X" && B2.Text == "X" && A3.Text == "X") 
+            {
+                MessageBox.Show("Player 'X' wins!");
+            }
+
+            if (A1.Text == "X" && B1.Text == "X" && C1.Text == "X")
+            {
+                MessageBox.Show("Player 'X' wins!");
+            }
+
+            if (A2.Text == "X" && B2.Text == "X" && C2.Text == "X")
+            {
+                MessageBox.Show("Player 'X' wins!");
+            }
+
+            if (A3.Text == "X" && B3.Text == "X" && C3.Text == "X")
+            {
+                MessageBox.Show("Player 'X' wins!");
+            }
+        }
+
+        public void Listjes()
+        {
+            Alghoritmes.Add(B1.Text);
+            Alghoritmes.Add(B2.Text);
+            Alghoritmes.Add(B3.Text);
         }
 
         public void WinnerOrNot()
         {
-
+            
         }
     }
 }
