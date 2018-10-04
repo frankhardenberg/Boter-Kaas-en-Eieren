1041deeabff4ef034e3f29889bf756d7130b4e0f
Frank Otten <34128958+frankhardenberg@users.noreply.github.com>
2018-10-04 12:26:41 +0200

    Update
    Update

--- Boter-Kaas-en-Eieren/TicTacToe.cs
+++ Boter-Kaas-en-Eieren/TicTacToe.cs
@@ -15,6 +15,7 @@ namespace Boter_Kaas_en_Eieren
         public TicTacToe()
         {
             InitializeComponent();
+            this.KeyPreview = true;            
         }
 
         bool Turn = true;
@@ -24,6 +25,7 @@ namespace Boter_Kaas_en_Eieren
         private void Letter_Click(object sender, EventArgs e)
         {
             Button Klik = (Button)sender;
+
             if (Turn)
             {
                 Klik.Text = "X";
@@ -50,15 +52,35 @@ namespace Boter_Kaas_en_Eieren
         }
 
         private void newGameF2ToolStripMenuItem_Click(object sender, EventArgs e)
-        {
-            TurnCount = 0;
-            ClearAllFields();
-            EnableAllButtons();            
+        {            
+            NewGame();
         }
 
         private void quitF4ToolStripMenuItem_Click(object sender, EventArgs e)
         {            
             Application.Exit();
+        }        
+
+        void TicTacToe_Keypress(object TicTacToe, KeyPressEventArgs e)
+        {
+            if (e.KeyChar == (char)Keys.F2)
+            {
+                e.Handled = true;
+                NewGame();
+            }
+
+            if (e.KeyChar == (char)Keys.F4)
+            {
+                e.Handled = true;
+                Application.Exit();
+            }
+        }
+
+        public void NewGame()
+        {
+            TurnCount = 0;
+            ClearAllFields();
+            EnableAllButtons();
         }
 
         public void WinnaarsCheck()
@@ -67,84 +89,98 @@ namespace Boter_Kaas_en_Eieren
             {
                 Winner = true;
                 MessageBox.Show("Player 'X' wins!");
+                DisableAllButtons();
             }
 
             if (B1.Text == "X" && B2.Text == "X" && B3.Text == "X")
             {
                 Winner = true;
                 MessageBox.Show("Player 'X' wins!");
+                DisableAllButtons();
             }
 
             if (C1.Text == "X" && C2.Text == "X" && C3.Text == "X")
             {
                 Winner = true;
                 MessageBox.Show("Player 'X' wins!");
+                DisableAllButtons();
             }
 
             if (A1.Text == "X" && B2.Text == "X" && C3.Text == "X" || C1.Text == "X" && B2.Text == "X" && A3.Text == "X")
             {
                 Winner = true;
                 MessageBox.Show("Player 'X' wins!");
+                DisableAllButtons();
             }
 
             if (A1.Text == "X" && B1.Text == "X" && C1.Text == "X")
             {
                 Winner = true;
                 MessageBox.Show("Player 'X' wins!");
+                DisableAllButtons();
             }
 
             if (A2.Text == "X" && B2.Text == "X" && C2.Text == "X")
             {
                 Winner = true;
                 MessageBox.Show("Player 'X' wins!");
+                DisableAllButtons();
             }
 
             if (A3.Text == "X" && B3.Text == "X" && C3.Text == "X")
             {
                 Winner = true;
                 MessageBox.Show("Player 'X' wins!");
+                DisableAllButtons();
             }
 
             if (A1.Text == "O" && A2.Text == "O" && A3.Text == "O")
             {
                 Winner = true;
                 MessageBox.Show("Player 'O' wins!");
+                DisableAllButtons();
             }
 
             if (B1.Text == "O" && B2.Text == "O" && B3.Text == "O")
             {
                 Winner = true;
                 MessageBox.Show("Player 'O' wins!");
+                DisableAllButtons();
             }
 
             if (C1.Text == "O" && C2.Text == "O" && C3.Text == "O")
             {
                 Winner = true;
                 MessageBox.Show("Player 'O' wins!");
+                DisableAllButtons();
             }
 
             if (A1.Text == "O" && B2.Text == "O" && C3.Text == "O" || C1.Text == "O" && B2.Text == "O" && A3.Text == "O")
             {
                 Winner = true;
                 MessageBox.Show("Player 'O' wins!");
+                DisableAllButtons();
             }
 
             if (A1.Text == "O" && B1.Text == "O" && C1.Text == "O")
             {
                 Winner = true;
                 MessageBox.Show("Player 'O' wins!");
+                DisableAllButtons();
             }
 
             if (A2.Text == "O" && B2.Text == "O" && C2.Text == "O")
             {
                 Winner = true;
                 MessageBox.Show("Player 'O' wins!");
+                DisableAllButtons();
             }
 
             if (A3.Text == "O" && B3.Text == "O" && C3.Text == "O")
             {
                 Winner = true;
                 MessageBox.Show("Player 'O' wins!");
+                DisableAllButtons();
             }
         }
 
@@ -159,6 +195,19 @@ namespace Boter_Kaas_en_Eieren
             C1.Enabled = true;
             C2.Enabled = true;
             C3.Enabled = true;
+        }
+
+        public void DisableAllButtons()
+        {
+            A1.Enabled = false;
+            A2.Enabled = false;
+            A3.Enabled = false;
+            B1.Enabled = false;
+            B2.Enabled = false;
+            B3.Enabled = false;
+            C1.Enabled = false;
+            C2.Enabled = false;
+            C3.Enabled = false;
         }
 
         public void ClearAllFields()
